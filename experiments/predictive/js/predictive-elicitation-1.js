41d1cb1eb0637bf60c8f198af6a5a2bc2f70d19f
mhtess <mtessler@stanford.edu>
2016-06-01 11:12:18 -0700

    add uniq turker for elicitation

--- experiments/predictive/js/predictive-elicitation-1.js
+++ experiments/predictive/js/predictive-elicitation-1.js
@@ -459,15 +459,15 @@ function make_slides(f) {
 /// init ///
 function init() {
 
-  // repeatWorker = false;
-  // (function(){
-  //     var ut_id = "mht-generics-20160524a";
-  //     if (UTWorkerLimitReached(ut_id)) {
-  //       $('.slide').empty();
-  //       repeatWorker = true;
-  //       alert("You have already completed the maximum number of HITs allowed by this requester. Please click 'Return HIT' to avoid any impact on your approval rating.");
-  //     }
-  // })();
+  repeatWorker = false;
+  (function(){
+      var ut_id = "mht-generics-20160601";
+      if (UTWorkerLimitReached(ut_id)) {
+        $('.slide').empty();
+        repeatWorker = true;
+        alert("You have already completed the maximum number of HITs allowed by this requester. Please click 'Return HIT' to avoid any impact on your approval rating.");
+      }
+  })();
 
 
   // var prev_levels = ["0","0","0.33","0.33","0.66","0.66","1","1"];
